4428e1f7c53880ff26bb0679de5a8b97036d0acc
Junxiao Shi <git@mail1.yoursunny.com>
2022-08-08 18:20:16 +0000

    include more Interest/Data fields in JSON output

--- record.go
+++ record.go
@@ -35,22 +35,34 @@ type Record struct {
 	Flow      []byte `json:"flow"`  // flow key
 	Size2     int    `json:"size2"` // packet size at NDNLPv2 layer
 
-	Size3       int      `json:"size3,omitempty"`      // packet size at L3
-	NackReason  uint8    `json:"nackReason,omitempty"` // Nack reason
-	Name        ndn.Name `json:"name,omitempty"`       // packet name
-	CanBePrefix bool     `json:"cbp,omitempty"`        // Interest CanBePrefix
-	MustBeFresh bool     `json:"mbf,omitempty"`        // Interest MustBeFresh
-	FinalBlock  bool     `json:"finalBlock,omitempty"` // Data is final block
+	Size3       int        `json:"size3,omitempty"`       // packet size at L3
+	NackReason  int        `json:"nackReason,omitempty"`  // Nack reason
+	Name        ndn.Name   `json:"name,omitempty"`        // packet name
+	CanBePrefix bool       `json:"cbp,omitempty"`         // Interest CanBePrefix
+	MustBeFresh bool       `json:"mbf,omitempty"`         // Interest MustBeFresh
+	FwHint      []ndn.Name `json:"fwHint,omitempty"`      // Interest ForwardingHint
+	Lifetime    int        `json:"lifetime,omitempty"`    // Interest InterestLifetime (ms)
+	HopLimit    int        `json:"hopLimit,omitempty"`    // Interest HopLimit
+	ContentType int        `json:"contentType,omitempty"` // Data ContentType
+	Freshness   int        `json:"freshness,omitempty"`   // Data FreshnessPeriod (ms)
+	FinalBlock  bool       `json:"finalBlock,omitempty"`  // Data is final block
 }
 
+// SaveInterest saves Interest/Nack fields on this Record.
 func (rec *Record) SaveInterest(interest ndn.Interest, nackReason uint8) {
 	rec.Name = interest.Name
 	rec.CanBePrefix = interest.CanBePrefix
 	rec.MustBeFresh = interest.MustBeFresh
-	rec.NackReason = nackReason
+	rec.FwHint = interest.ForwardingHint
+	rec.Lifetime = int(interest.Lifetime.Milliseconds())
+	rec.HopLimit = int(interest.HopLimit)
+	rec.NackReason = int(nackReason)
 }
 
+// SaveData saves Data fields on this Record.
 func (rec *Record) SaveData(data ndn.Data) {
 	rec.Name = data.Name
+	rec.ContentType = int(data.ContentType)
+	rec.Freshness = int(data.Freshness.Milliseconds())
 	rec.FinalBlock = data.IsFinalBlock()
 }
